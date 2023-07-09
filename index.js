f0d228c691d942254fae37c4109bd0c2885334c0
Bynio <bynio788@gmail.com>
2023-07-09 10:23:49 +0200

    feat: scrap providers list

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ async function start(){
     const info = await page.$eval('div.page__subtitle', el => el.textContent);
     console.log(info)
 
+    const ProviderListLink = await page.evaluate(() => {
+        const providerElements = Array.from(document.querySelectorAll('.rankingProvider__item')).slice(0, 4);
+        
+        return providerElements.map(element => {
+            const providerName = element.querySelector('img').getAttribute('alt');
+            const providerLink = element.querySelector('a').getAttribute('href');
+            return { providerName, providerLink }
+        })
+    })
+
+    console.log(ProviderListLink)
+
     await browser.close();
 }
 
