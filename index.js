485b17aa8407b426fb904e589a6cfb0d8f635a22
Bynio <bynio788@gmail.com>
2023-07-09 11:01:14 +0200

    feat: scrap top 10 movies of actual year

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import puppeteer from "puppeteer";
 
-
+const baseUrl = 'https://www.filmweb.pl'
 async function start() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.filmweb.pl/ranking/vod/film')
+    await page.goto(`${baseUrl}/ranking/vod/film`)
 
     const ProviderListLink = await page.evaluate(() => {
         const providerElements = Array.from(document.querySelectorAll('.rankingProvider__item')).slice(0, 4);
@@ -16,32 +16,37 @@ async function start() {
         })
     })
 
-    await Promise.all([
-        page.click('.rankingProvider__item a'),
-        page.waitForNavigation()
-    ])
+    const x = []
+    for(const link of ProviderListLink){
+        await Promise.all([
+            page.goto(`${baseUrl}${link.providerLink}`),
+            page.waitForNavigation()
+        ])
+    
+        await page.click('.rankingHeader__filter')
+        await page.click('button[data-placeholder="Wybierz rok produkcji"]')
+        await page.click('label[data-name="2023"].filterSelect__option')
+    
+        const info = await page.$eval('div.page__subtitle', el => el.textContent);
+        console.log(info)
+    
+        const movies = await page.evaluate(() => {
+            const movieElements = [...document.querySelectorAll('div.rankingType[itemprop="itemListElement"]')]
+    
+            const movieSliced = movieElements.slice(0, 10)
+            return movieSliced.map(element => {
+                const rating = element.querySelector('.rankingType__rate--value').innerText;
+                const name = element.querySelector('.rankingType__title a').innerText
+    
+                return { rating, name }
+            })
+        });
+        x.push(movies)
+        console.log(movies)
+    }
 
-    await page.click('.rankingHeader__filter')
-    await page.click('button[data-placeholder="Wybierz rok produkcji"]')
-    await page.click('label[data-name="2023"].filterSelect__option')
 
-    const info = await page.$eval('div.page__subtitle', el => el.textContent);
-    console.log(info)
-
-    const movies = await page.evaluate(() => {
-        const movieElements = [...document.querySelectorAll('div.rankingType[itemprop="itemListElement"]')]
-        // const  = []
-
-        const movieSliced = movieElements.slice(0, 10)
-        return movieSliced.map(element => {
-            const rating = element.querySelector('.rankingType__rate--value').innerText;
-            const name = element.querySelector('.rankingType__title a').innerText
-
-            return { rating, name }
-        })
-    });
-    console.log(movies)
-
+    console.log(x)
     await browser.close();
 }
 
